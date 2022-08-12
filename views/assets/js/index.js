905c082e3a892487968c289b857319e05ca308ce
gadhagod <69025547+gadhagod@users.noreply.github.com>
2022-08-12 14:57:20 -0700

    Styling improvements

--- views/assets/js/index.js
+++ views/assets/js/index.js
@@ -45,7 +45,11 @@ class Themes {
             document.querySelector(".table-head").classList.remove("bg-secondary");
         }
         document.querySelectorAll(".form-control").forEach((elem) => {
-            elem.classList.remove("bg-dark");
+            if (elem.disabled) {
+                elem.style.background = "";
+            } else {
+                elem.classList.remove("bg-dark");
+            }
         });
         this.#toggleButton.classList.remove("bi-sun");
         this.#toggleButton.classList.add("bi-moon");
@@ -68,7 +72,11 @@ class Themes {
             document.querySelector(".table-head").classList.add("bg-secondary");
         }
         document.querySelectorAll(".form-control").forEach((elem) => {
-            elem.classList.add("bg-dark");
+            if (elem.disabled) {
+                elem.style.background = "#3b3b3b";
+            } else {
+                elem.classList.add("bg-dark");
+            }
         });
         this.#toggleButton.classList.remove("bi-moon");
         this.#toggleButton.classList.add("bi-sun");
